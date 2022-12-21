832d8a2a4f028929f4ac8393d214362bbf4a45c5
Nikolay Bayborodin <nbbrdn@gmail.com>
2022-12-21 16:57:39 +1000

    Solve the day 6 part 1

---
+++ 2022/day06/day06-a.py
@@ -0,0 +1,32 @@
+import os
+from pathlib import Path
+
+DATA_FILE = 'input.txt'
+MARKER_LENGTH = 4
+
+
+def find_marker(stream :str) -> int:
+    curr_pos = MARKER_LENGTH
+    stream_length = len(stream)
+
+    while curr_pos < stream_length:
+        stream_slice = stream[curr_pos - MARKER_LENGTH:curr_pos]
+        if len(set(stream_slice)) == len(stream_slice):
+            return curr_pos
+        curr_pos += 1
+
+    return -1
+
+
+def main() -> None:
+    script_dir = Path(__file__).parent.absolute()
+    file_name = os.path.join(script_dir, DATA_FILE)
+
+    with open(file_name) as f:
+        data_stream = f.read()
+    
+    ch_cnt = find_marker(data_stream)
+    print(ch_cnt)
+
+if __name__ == '__main__':
+    main()
